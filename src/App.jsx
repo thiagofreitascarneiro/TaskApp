cd29581cd9aed36572fcf90d0dfb064acb252efb
thiago <thiagofreitascarneiro@hotmail.com>
2021-11-02 11:57:52 -0300

    creating button taskDetails to view details the task.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 
 import {v4 as uuidv4 } from 'uuid';
-import { BrowserRouter as Router, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask';
-import "./App.css"
+import "./App.css";
 import Header from './components/Header';
+import TaskDetails from './components/TaskDetails'
 
 const App = () => {
     const [tasks, setTasks] = useState([
@@ -57,26 +58,26 @@ const App = () => {
       <Router>
         <div className="container">
             <Header />
-            <Route path="/" exact render={() => (
-                <>
-                  <AddTask handleTaskAddition={handleTaskAddition}/> 
-                  <Tasks 
-                      tasks={tasks} 
-                      handleTaskClick={handleTaskClick} 
-                      handleTaskDeletion={handleTaskDeletion}  
-                  />
-                </>
-            )}
+            <Route path="/" 
+                  exact 
+                  render={() => (
+                    <>
+                      <AddTask handleTaskAddition={handleTaskAddition} /> 
+                      <Tasks 
+                          tasks={tasks} 
+                          handleTaskClick={handleTaskClick} 
+                          handleTaskDeletion={handleTaskDeletion}  
+                      />
+                    </>
+                  )}
             />
-            {/*<AddTask handleTaskAddition={handleTaskAddition}/> 
-            <Tasks 
-              tasks={tasks} 
-              handleTaskClick={handleTaskClick} 
-              handleTaskDeletion={handleTaskDeletion}              
-    / > */} 
+          <Route path="/:taskTitle" exact render={TaskDetails} />
         </div>
       </Router>
-  );
+    );
 };
 
+
 export default App;
+
+
---
+++ src/components/TaskDetails.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Button from './Button'
+
+const TaskDetails = () => {
+    const params = 
+    return (
+        <>
+            <div className="back-button-container">
+                <Button>back to page</Button>
+            </div>
+            <div className="task-details-container">
+                <p></p>
+                <p>
+                    Loreim ipsun, bobbv asdfs\dfsadg dsagasg     
+                </p>
+            </div>
+        </>
+    );
+};
+
+export default TaskDetails;
